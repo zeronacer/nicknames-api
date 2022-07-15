bcfc3e14fa11312f47a49913f1994cea8b9c8664
mgrabau <maximilian.grabau@lynet.de>
2022-07-15 14:10:03 +0200

    Add cors

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 
 const fs = require('fs');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 // routes
 // informative base route
